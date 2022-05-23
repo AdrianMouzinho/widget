bb2f4c8fe7ff439a1e8ff68270680ebb95309f9c
adria mouzinho <adrianmouzinhopro@gmail.com>
2022-05-23 11:56:58 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          400: '#996DFF',
+          500: '#8257E5'
+        },
+        text: {
+          400: '#f4f4f5',
+          500: '#a1a1aa'
+        },
+        surface: {
+          400: '#3F3F46',
+          400: '#27272A',
+          500: '#18181B'
+        },
+        stroke: {
+
+        },
+        dark: '#09090A'
+      }
+    },
+  },
+  plugins: [],
+}
